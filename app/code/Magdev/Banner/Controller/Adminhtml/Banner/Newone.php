278e1fc7a72ae56a7340f9aa401fbabd4bbb2ec8
vvv <varun.20010@gmail.com>
2025-11-06 20:59:30 +0530

    new controller

---
+++ app/code/Magdev/Banner/Controller/Adminhtml/Banner/Newone.php
@@ -0,0 +1,26 @@
+<?php
+namespace Magdev\Banner\Controller\Adminhtml\Banner;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\View\Result\PageFactory;
+
+class Newone extends Action
+{
+    const ADMIN_RESOURCE = 'Magdev_Banner::banner';
+
+    protected $resultPageFactory;
+
+    public function __construct(Action\Context $context, PageFactory $resultPageFactory)
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Magdev_Banner::banner');
+        $resultPage->getConfig()->getTitle()->prepend(__('Banners'));
+        return $resultPage;
+    }
+}
